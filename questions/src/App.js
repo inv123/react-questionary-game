40c3e206b2145c84092b330d7f73b34781235677
inv123 <nedyalko.tiholov@yahoo.com>
2024-01-05 16:46:47 +0200

    add header

--- questions/src/App.js
+++ questions/src/App.js
@@ -1,10 +1,11 @@
-
-
+import './style/style.css';
+import { Header } from './components/Header';
+ 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="app">
+      <Header />
+    </div>
   );
 }
 
---
+++ questions/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../asset/logo.jpg';
+
+export const Header = () => {
+  return (
+    <>
+        <header>
+            <img src={logo} />
+        </header>
+    </>
+  )
+}
